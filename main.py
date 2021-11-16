2d40c2b732a2924c3491b6c7683536f91f1ec2fe
Mayank18-dotcom <mayank123chittora@gmail.com>
2021-11-16 10:28:35 +0530

    no-cache

--- main.py
+++ main.py
@@ -53,6 +53,17 @@ def scheduleCall(data):
         sched.start()
     
 app = Flask(__name__)
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 @app.route('/')
 def home_page():
     return redirect(url_for('welcome'))
